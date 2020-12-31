6b0a23510b9e79735311e7a9cc439e5a9fa51ac1
hawa <hj1172@nyu.edu>
2020-12-30 22:27:31 -0500

    learning how to uses data in the backend

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def hello():
     name = 'Hawa Jalloh'
-    
-    return render_template('index.html', data=name)
+    username = 'hawa03'
+    return render_template('index.html', data=name, data2 = username)
+
     
 if __name__ == '__main__':
     app.run()
